1accc0a120f0cac5a75a34a84601cf273869f335
lhs252 <usbt38055@naver.com>
2023-03-09 11:14:45 +0000

    end

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-
 def twoSum(nums: list[int], target: int) -> list[int]:
-    a = nums[0]
     check = 0
-    i = 0
-    j = 0
     oa = 0
     ob = 0
-    while check != 1 :
-        if i + 1 >= len(nums):
-            break
-        if nums[j] + nums[i + 1] == target:
-            oa = j
-            ob = i + 1
-            check = 1
-        else :
-            j = j + 1
-            i = i + 1
+    while check != 1:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target :
+                    oa = i
+                    ob = j
+		    check = 1
     return [oa, ob]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
